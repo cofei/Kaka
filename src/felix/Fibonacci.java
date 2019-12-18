6161a812746a1466e8a9bd8c3c8ac96665ad93d0
cofei <cofei@outlook.com>
2019-12-18 15:32:54 +0800

    implement Fibonacci with Java.

--- src/felix/Fibonacci.java
+++ src/felix/Fibonacci.java
@@ -1,5 +1,29 @@
 package felix;
 
-public class Fibonacci {
+import java.math.*;
 
+public class Fibonacci {
+	private BigInteger fibnum;		// the final Fibonacci number;
+	private BigInteger pfib;		// previous Fibonacci number;
+	private BigInteger ppfib;		// previous previous Fibonacci number;
+	
+	public BigInteger Sequence(int inNum) {
+		ppfib = BigInteger.valueOf(1);
+		pfib = BigInteger.valueOf(1);
+		
+		if (inNum <= 2) {
+			fibnum = BigInteger.valueOf(1);
+		} else {
+			for (int i = 3; i <= inNum; i++) {
+				fibnum = pfib.add(ppfib);
+				ppfib = pfib;
+				pfib = fibnum;
+			}
+		}
+		
+		 System.out.println("Fibonacci Series of "+inNum+" numbers: " + fibnum);
+		return fibnum;
+		
+	}
+	
 }
--- src/felix/Kaka.java
+++ src/felix/Kaka.java
@@ -3,7 +3,14 @@ package felix;
 public class Kaka {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world!");
+		System.out.println("Hello world!");
+		
+		Fibonacci fib = new Fibonacci();
+		
+		fib.Sequence(10);
+		fib.Sequence(20);
+		fib.Sequence(36);
+		
 	}
 
 }
